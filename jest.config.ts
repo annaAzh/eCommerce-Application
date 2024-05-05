8be8e4800f16d1efef979bee4a0ab511a580f96c
rRedq <redq_000@mail.ru>
2024-05-05 22:26:35 +0500

    chore: change jest config

--- jest.config.ts
+++ jest.config.ts
@@ -2,10 +2,6 @@ import type { Config } from 'jest';
 
 const config: Config = {
   preset: 'ts-jest',
-  clearMocks: true,
-  collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
   testEnvironment: "jsdom",
 };
 
