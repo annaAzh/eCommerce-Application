17018959614a064dbe657ab78b01d4e3550446f1
Irina Fileva <irinkaskai@gmail.com>
2024-05-08 22:03:30 +0500

    feat: add store

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,12 @@
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 import App from './app';
+import { store } from 'store/index';
 
 const root = createRoot(document.getElementById('root') as HTMLDivElement);
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+);
