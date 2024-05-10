cda7f04ac2ef53c8e63ebf55b0ab3661b7987e24
Irina Fileva <irinkaskai@gmail.com>
2024-05-10 15:45:13 +0500

    feat: add router provider

---
+++ src/app/providers/routerProvider/ui/routeProvider.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Router, rootRouter } from '../config/rootRouter';
+
+export const RouteProvider = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {rootRouter.map((route: Router) =>
+          <Route key={route.path} path={route.path} element={route.element}></Route>
+        )}
+      </Routes>
+    </BrowserRouter>
+  );
+};
