dec858751f3e0c05f6d4f245e1c4658d95878a8c
Irina Fileva <irinkaskai@gmail.com>
2024-05-10 15:49:46 +0500

    refactor: correct paths

--- src/shared/lib/hooks/useAppSelect/useAppSelect.ts
+++ src/shared/lib/hooks/useAppSelect/useAppSelect.ts
@@ -1,4 +1,4 @@
 import { useSelector, TypedUseSelectorHook } from 'react-redux';
-import { RootState } from 'app/Providers/StoreProvider';
+import { RootState } from 'app/providers/storeProvider';
 
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
