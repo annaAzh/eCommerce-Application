f87438ba4e027e731672db80647bba16cdbe76cf
Irina Fileva <irinkaskai@gmail.com>
2024-05-12 01:40:34 +0500

    feat: add a registration form to the page

--- src/pages/registration/Registration.tsx
+++ src/pages/registration/Registration.tsx
@@ -1,9 +1,6 @@
+import { RegistrationForm } from 'features/RegistrationUser';
 import { FC } from 'react';
 
 export const Registration: FC = () => {
-  return (
-    <div>
-      <h1>Page Registration</h1>
-    </div>
-  );
+  return <RegistrationForm />;
 };
