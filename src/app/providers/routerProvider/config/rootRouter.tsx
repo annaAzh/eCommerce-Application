4f7239cffff1836aba7ffc3b9a4c05711e6d89c7
IrinaFileva <134499035+IrinaFileva@users.noreply.github.com>
2024-05-11 16:30:36 +0500

    feat/RSS-ECOMM-2_19: implement direct navigation links to main, login, and registration pages for unauthorized users (#83)
    * chore: install react-router-dom
    
    * feat: add layouts for main, login and registration pages
    
    * feat: add a root router
    
    * feat: add router provider
    
    * feat: embed the router in the application
    
    * refactor: correct paths
    
    * feat: add a login form to the page
    
    * refactor: case change
    
    * fix: correct lint errors

---
+++ src/app/providers/routerProvider/config/rootRouter.tsx
@@ -0,0 +1,34 @@
+import { Login } from 'pages/login/Login';
+import { Main } from 'pages/main/Main';
+import { Registration } from 'pages/registration/Registration';
+
+enum Paths {
+  start = '/',
+  main = 'main',
+  login = 'login',
+  registration = 'registration',
+}
+
+export interface Router {
+  path: Paths;
+  element: JSX.Element;
+}
+
+export const rootRouter: Router[] = [
+  {
+    path: Paths.start,
+    element: <Main />,
+  },
+  {
+    path: Paths.main,
+    element: <Main />,
+  },
+  {
+    path: Paths.login,
+    element: <Login />,
+  },
+  {
+    path: Paths.registration,
+    element: <Registration />,
+  },
+];
