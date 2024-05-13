e78de140f7afba87ed34e10192cdd1191d75cab7
IrinaFileva <134499035+IrinaFileva@users.noreply.github.com>
2024-05-13 20:25:17 +0500

    Feat/RSS-ECOMM-2_18: ensure all links on the main page correctly redirect to their respective pages (#88)
    * feat: add a registration form to the page
    
    * feat: add an image to the main page
    
    * fix: problem correction
    
    * fix: error lint

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,9 +1,11 @@
 import { FC } from 'react';
+import ImgKittens from 'shared/assets/img/kittensMain.png';
+import './Main.css';
 
 export const Main: FC = () => {
   return (
-    <div>
-      <h1>Page Main</h1>
+    <div className="wrapper-main">
+      <img className="img-kittens" src={ImgKittens} alt="kittens"></img>
     </div>
   );
 };
