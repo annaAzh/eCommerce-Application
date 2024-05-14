1d213a22d1d2429e9dcb9dc4117cd6e11d99a833
rRedq <redq_000@mail.ru>
2024-05-14 14:36:24 +0500

    refactor: replace comment

--- src/features/LoginUser/model/services/requestLogin.ts
+++ src/features/LoginUser/model/services/requestLogin.ts
@@ -31,31 +31,3 @@ export const requestLogin = createAsyncThunk('login/requestLoginToken', async (l
     return thunkAPI.rejectWithValue(errorMsg);
   }
 });
-
-// export const requestRefreshToken = createAsyncThunk(
-//   'token/requestLoginToken',
-//   async (loginData: LoginData, thunkAPI) => {
-//     try {
-//       const { username, password, token } = loginData;
-
-//       const data = { email: username, password: password };
-//       const res = await axios.post<RefreshTokenSucces>(`${process.env.PROJECT_KEY}/login`, data, {
-//         baseURL: `${process.env.API_URL}`,
-//         // headers: { Authorization: `Bearer 1mM1MZX-GiCV3ORrl5JPM4eAM2zZO4iZ`, 'Content-Type': 'application/json' },
-//         headers: { Authorization: `Bearer ${token}`, 'Content-Type': 'application/json' },
-//       });
-//       const success: RefreshTokenSucces = res.data;
-//       return success;
-//     } catch (error) {
-//       let errorMsg = 'damn error';
-//       if (error instanceof Error) {
-//         const reject: ErrorWithResponse = error as ErrorWithResponse;
-//         if (reject.response && reject.response.data) {
-//           const errorResponse: RefreshTokenReject = reject.response.data as RefreshTokenReject;
-//           errorMsg = errorResponse.message;
-//         }
-//       }
-//       return thunkAPI.rejectWithValue(errorMsg);
-//     }
-//   },
-// );
