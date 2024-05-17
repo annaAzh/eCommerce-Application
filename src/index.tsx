8a12f60e36a57c5f453ed79a56177afbbdd2bfd5
IrinaFileva <134499035+IrinaFileva@users.noreply.github.com>
2024-05-17 22:52:36 +0500

    fix/RSS-ECOMM-2_17: change the number of links on the home page (#101)
    * feat: add layouts for the Cart, Profile and Catalog pages
    
    * fix: change the router's connection location and add paths to it
    
    * feat: add header links
    
    * feat: add links to all feature pages to the main page
    
    * fix: change styles for adaptive

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
 import { createRoot } from 'react-dom/client';
 import { StoreProvider } from 'app/providers/storeProvider';
 import { App } from 'app/App';
+import { BrowserRouter } from 'react-router-dom';
+import { Header } from 'widgets/Header';
+import { NotificationTool } from 'entities/NotificationTool';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <StoreProvider>
-    <App />
-  </StoreProvider>,
+  <BrowserRouter>
+    <StoreProvider>
+      <NotificationTool />
+      <Header />
+      <App />
+    </StoreProvider>
+  </BrowserRouter>,
 );
---
+++ src/shared/types/routerTypes.ts
@@ -0,0 +1,10 @@
+export enum Paths {
+  start = '/',
+  main = 'main',
+  login = 'login',
+  registration = 'registration',
+  catalog = 'catalog',
+  card = 'card',
+  profile = 'profile',
+  notFound = '*',
+}
