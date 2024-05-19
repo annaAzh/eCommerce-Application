4127a8a86665eded17b860448d74275a29c7b473
annaAzh <forarre@yandex.ru>
2024-05-19 16:11:33 +0300

    feat/RSS-ECOMM-2_24: add css modules

--- webpack.config.ts
+++ webpack.config.ts
@@ -27,7 +27,23 @@ module.exports = (env: EnvVar) => {
     module: {
       rules: [
         {
+          test: /\.module\.css$/i,
+          use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                modules: {
+                  localIdentName: isDev ? '[path][name]__[local]--[hash:base64:5]' : '[hash:base64:8]',
+                  namedExport: false,
+                },
+              },
+            },
+          ],
+        },
+        {
           test: /\.css$/i,
+          exclude: /\.module\.css$/i,
           use: [isDev ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader'],
         },
         {
