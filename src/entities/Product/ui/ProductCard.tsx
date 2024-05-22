ebd2640af323429d0b44b253032e83b84cb9b4f7
rRedq <redq_000@mail.ru>
2024-05-22 18:41:04 +0500

    refactor: change some styles for product cards

--- src/entities/Product/ui/ProductCard.tsx
+++ src/entities/Product/ui/ProductCard.tsx
@@ -2,14 +2,17 @@ import { Product } from '../model/types/productTypes';
 import styles from './ProductCard.module.css';
 
 const ProductCard = ({ product }: { product: Product }): JSX.Element => {
-  const { images, description, name, id } = product;
+  const { images, description, name } = product;
+  const firstImage = images[0];
   return (
     <div className={styles.card}>
       <div className={styles.imageCover}>
-        <img className={styles.image} src={images[0]} alt={id} />
+        <img className={styles.image} src={firstImage} />
       </div>
-      <div>{name}</div>
-      <div className={styles.description}>{description}</div>
+      <div className={styles.descriptionCover}>
+        <p className={styles.name}>{name}</p>
+        <p className={styles.description}>{description}</p>
+      </div>
     </div>
   );
 };
