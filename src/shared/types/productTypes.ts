45f4571a5ae992dcdd4914fb03a10e025dfbcd14
Irina Fileva <irinkaskai@gmail.com>
2024-05-26 17:03:53 +0500

    feat: add a link to the product page, move types, add a property to the product

--- src/shared/types/productTypes.ts
+++ src/shared/types/productTypes.ts
@@ -1,11 +1,37 @@
 interface Product {
   id: string;
+  key: string;
   name: string;
   description: string;
   images: string[];
   prices: FormattedPrice;
 }
 
+type Prices = {
+  discounted?: {
+    value: {
+      centAmount: number;
+      currencyCode: CurrencyCodes;
+      fractionDigits: number;
+    };
+  };
+  value?: {
+    centAmount: number;
+    currencyCode: CurrencyCodes;
+    fractionDigits: number;
+  };
+};
+
+type CurrencyCodes = 'USD';
+
+type Images = {
+  dimensions: {
+    h: number;
+    w: number;
+  };
+  url: string;
+};
+
 type FormattedPrice = { currentPrice: string; discountedPrice?: string };
 
-export { Product, FormattedPrice };
+export { Product, Prices, Images, FormattedPrice };
