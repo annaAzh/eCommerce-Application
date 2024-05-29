59399a49aef4aec64d5d2e5108e2f9b91dfe2f61
rRedq <redq_000@mail.ru>
2024-05-29 18:42:40 +0500

    feat: add left side filter

--- src/pages/catalog/Catalog.tsx
+++ src/pages/catalog/Catalog.tsx
@@ -1,14 +1,19 @@
 import { FC } from 'react';
 import { FilterList } from 'widgets/FilterList';
+import { LeftSideFilter } from 'widgets/LeftSideFilter';
 import { NavMenu } from 'widgets/NavMenu';
 import { ProductList } from 'widgets/ProductList';
+import style from './Catalog.module.css';
 
 export const Catalog: FC = () => {
   return (
     <>
       <NavMenu />
       <FilterList />
-      <ProductList />
+      <div className={style.cover}>
+        <LeftSideFilter />
+        <ProductList />
+      </div>
     </>
   );
 };
---
+++ src/widgets/LeftSideFilter/ui/LeftSideFilter.tsx
@@ -0,0 +1,42 @@
+import { FC } from 'react';
+import style from './LeftSideFilter.module.css';
+import { addSearchCategory, getAllCategories, getSearchQuery } from 'entities/Product';
+import { useAppDispatch, useAppSelector } from 'shared/lib/hooks';
+
+export const LeftSideFilter: FC = () => {
+  const dispatch = useAppDispatch();
+  const categories = useAppSelector(getAllCategories);
+  const result = useAppSelector(getSearchQuery)?.categoriesId;
+  const categoriesId = result?.split('"')[1];
+
+  const onClick = (value: string) => {
+    dispatch(addSearchCategory({ categoriesId: `categories.id:"${value}"` }));
+  };
+
+  console.log(categoriesId);
+  return (
+    <div className={style.cover}>
+      {categories.map(({ id, name, subCategory }) => (
+        <div key={id}>
+          <div
+            className={categoriesId === id ? `${style.active} ${style.field}` : `${style.field}`}
+            onClick={() => onClick(id)}
+          >
+            {name}
+          </div>
+          <div className={style.subCover}>
+            {subCategory.map((category) => (
+              <div
+                key={category.id}
+                className={categoriesId === category.id ? `${style.active} ${style.field}` : `${style.field}`}
+                onClick={() => onClick(category.id)}
+              >
+                {category.name}
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
