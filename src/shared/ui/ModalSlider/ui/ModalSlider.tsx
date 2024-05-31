63f4a4d3319a32ff4f3962c754ecf7370301b332
Irina Fileva <irinkaskai@gmail.com>
2024-06-01 01:56:21 +0500

    feat: add slider to modal window

--- src/shared/ui/ModalSlider/ui/ModalSlider.tsx
+++ src/shared/ui/ModalSlider/ui/ModalSlider.tsx
@@ -1,13 +1,21 @@
 import { Modal } from 'antd';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FreeMode, Navigation, Thumbs } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/navigation';
+import 'swiper/css/thumbs';
 import './ModalSlider.css';
 
 interface ModalProps {
   visible: boolean;
-  image: string;
+  activeImg: number | undefined;
+  images: string[];
   onClose: () => void;
 }
 
 export const ModalSlider = (props: ModalProps) => {
+  if (!props.visible) return null;
   return (
     <Modal
       open={props.visible}
@@ -23,7 +31,23 @@ export const ModalSlider = (props: ModalProps) => {
       onCancel={props.onClose}
       width={900}
     >
-      <img className="img-modal" src={props.image}></img>
+      <Swiper
+        loop={true}
+        spaceBetween={3}
+        navigation={true}
+        centeredSlides={true}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className="modalSwiper"
+        initialSlide={props.activeImg}
+      >
+        {props.images.map((image: string, index: number) => {
+          return (
+            <SwiperSlide key={index}>
+              <img src={image} />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </Modal>
   );
 };
