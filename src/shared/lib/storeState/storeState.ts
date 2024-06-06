16cc935a16e5597e3ad3de11ce5ed27e13e4a4c9
rRedq <redq_000@mail.ru>
2024-06-06 21:30:50 +0500

    feat: add anonymous user access

--- src/shared/lib/storeState/storeState.ts
+++ src/shared/lib/storeState/storeState.ts
@@ -1,19 +1,23 @@
-const STORE_KEY = 'token';
+type StoreKeys = 'token' | 'anonymous';
 
-const setLocalStoreState = (token: string): void => {
-  localStorage.setItem(STORE_KEY, JSON.stringify(token));
+const setLocalStoreState = (value: string, key: StoreKeys = 'token'): void => {
+  localStorage.setItem(key, JSON.stringify(value));
 };
 
-const getLocalStoreState = (): string => {
-  let response = localStorage.getItem(STORE_KEY) || '';
+const getLocalStoreState = (key: StoreKeys = 'token'): string => {
+  let response = localStorage.getItem(key) || '';
   if (response) {
     response = JSON.parse(response);
   }
   return response;
 };
 
+const deleteLocalStoreStateByKey = (key: StoreKeys): void => {
+  localStorage.removeItem(key);
+};
+
 const clearLocalStoreState = (): void => {
   localStorage.clear();
 };
 
-export { setLocalStoreState, getLocalStoreState, clearLocalStoreState };
+export { setLocalStoreState, getLocalStoreState, clearLocalStoreState, deleteLocalStoreStateByKey };
