21e71dc30dbbe2004b9720bca9c3e55ad9850299
rRedq <redq_000@mail.ru>
2024-06-06 21:31:44 +0500

    feat: add the ability to add goods to cart

--- src/features/ProductCard/ui/ProductCard.tsx
+++ src/features/ProductCard/ui/ProductCard.tsx
@@ -2,12 +2,33 @@ import { Product } from 'shared/types';
 import styles from './ProductCard.module.css';
 import onSale from 'shared/assets/img/onSale.svg';
 import parse from 'html-react-parser';
+import { PrimaryControlButton } from 'shared/ui';
+import { addToCart, createCart, getCart } from 'entities/Cart';
+import { getAccessToken } from 'entities/User';
+import { useAppDispatch, useAppSelector } from 'shared/lib/hooks';
+import { useEffect, useState } from 'react';
 
 const ProductCard = ({ product, onClick }: { product: Product; onClick: (key: string) => void }): JSX.Element => {
-  const { images, description, name, prices, key } = product;
+  const dispatch = useAppDispatch();
+  const token = useAppSelector(getAccessToken);
+  const cart = useAppSelector(getCart);
+  const { images, description, name, prices, key, id } = product;
   const { discountedPrice, currentPrice } = prices;
   const descMaxLength = 300;
   const firstImage = images[0];
+  const [productId, setProductId] = useState<string>();
+
+  useEffect(() => {
+    if (!token || !productId) return;
+
+    if (!cart.id) {
+      dispatch(createCart(token));
+    } else {
+      if (!cart.version) return;
+      dispatch(addToCart({ token, productId: id, cartId: cart.id, version: cart.version }));
+      setProductId(undefined);
+    }
+  }, [productId, cart]);
 
   return (
     <div className={styles.card} onClick={() => onClick(key)}>
@@ -23,6 +44,17 @@ const ProductCard = ({ product, onClick }: { product: Product; onClick: (key: st
               ? parse(description.slice(0, descMaxLength).concat('...'))
               : parse(description)}
           </p>
+          <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <PrimaryControlButton
+              onClick={(e) => {
+                e.stopPropagation();
+                setProductId(id);
+              }}
+            >
+              add
+            </PrimaryControlButton>
+          </div>
+
           {discountedPrice ? (
             <div>
               <div className={`${styles.commonPriceClass} ${styles.crossedPrice}`}>{currentPrice}</div>
