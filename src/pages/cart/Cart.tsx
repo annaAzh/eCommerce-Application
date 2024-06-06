8c669642d9714507a2f4f460740a4fd30eb0f9fe
annaAzh <forarre@yandex.ru>
2024-06-06 22:04:48 +0300

    feat: add route for cart page and cart icon at header

---
+++ src/pages/cart/Cart.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import style from './Cart.module.css';
+
+export const Cart: FC = () => {
+  return (
+    <>
+      <div className={style.cover}>
+        <h2>Cart page</h2>
+      </div>
+    </>
+  );
+};
--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -7,7 +7,14 @@ import { useAppSelector } from 'shared/lib/hooks/useAppSelect/useAppSelect';
 import { ButtonLogOut } from 'features/LogoutUser';
 import { Paths } from 'shared/types';
 import './Header.css';
-import { FormOutlined, HomeOutlined, LoginOutlined, ProductOutlined, UserOutlined } from '@ant-design/icons';
+import {
+  FormOutlined,
+  HomeOutlined,
+  LoginOutlined,
+  ProductOutlined,
+  ShoppingCartOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
 import { Tooltip } from 'antd';
 
 interface LinkOfPage {
@@ -32,6 +39,16 @@ const links: LinkOfPage[] = [
       <Link className="link-profile" to={Paths.catalog}>
         <Tooltip title="catalog">
           <ProductOutlined style={{ fontSize: '2rem' }} />
+        </Tooltip>
+      </Link>
+    ),
+  },
+  {
+    key: Paths.cart,
+    label: (
+      <Link className="link-profile" to={Paths.cart}>
+        <Tooltip title="cart">
+          <ShoppingCartOutlined style={{ fontSize: '2rem' }} />
         </Tooltip>
       </Link>
     ),
