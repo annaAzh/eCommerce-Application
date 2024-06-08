c9cd90c100206cdf171dda27a9c0f5cdd4c7fd94
rRedq <redq_000@mail.ru>
2024-06-09 00:00:01 +0500

    feat: add button 'add' on product card

--- src/features/ProductCard/ui/ProductCard.tsx
+++ src/features/ProductCard/ui/ProductCard.tsx
@@ -2,16 +2,18 @@ import { Product } from 'shared/types';
 import styles from './ProductCard.module.css';
 import onSale from 'shared/assets/img/onSale.svg';
 import parse from 'html-react-parser';
-import { PrimaryControlButton } from 'shared/ui';
-import { addToCart, createCart, getCart } from 'entities/Cart';
+import { GreenButtonWithPlus } from 'shared/ui';
+import { addToCart, createCart, getCart, getOriginalGoods } from 'entities/Cart';
 import { getAccessToken } from 'entities/User';
 import { useAppDispatch, useAppSelector } from 'shared/lib/hooks';
 import { useEffect, useState } from 'react';
+import { PulseLoader } from 'react-spinners';
 
 const ProductCard = ({ product, onClick }: { product: Product; onClick: (key: string) => void }): JSX.Element => {
   const dispatch = useAppDispatch();
   const token = useAppSelector(getAccessToken);
   const cart = useAppSelector(getCart);
+  const originalGoods = useAppSelector(getOriginalGoods);
   const { images, description, name, prices, key, id } = product;
   const { discountedPrice, currentPrice } = prices;
   const descMaxLength = 300;
@@ -30,6 +32,10 @@ const ProductCard = ({ product, onClick }: { product: Product; onClick: (key: st
     }
   }, [productId, cart]);
 
+  const handler = () => {
+    setProductId(id);
+  };
+
   return (
     <div className={styles.card} onClick={() => onClick(key)}>
       <p className={styles.name}>{name}</p>
@@ -44,17 +50,6 @@ const ProductCard = ({ product, onClick }: { product: Product; onClick: (key: st
               ? parse(description.slice(0, descMaxLength).concat('...'))
               : parse(description)}
           </p>
-          <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-            <PrimaryControlButton
-              onClick={(e) => {
-                e.stopPropagation();
-                setProductId(id);
-              }}
-            >
-              add
-            </PrimaryControlButton>
-          </div>
-
           {discountedPrice ? (
             <div>
               <div className={`${styles.commonPriceClass} ${styles.crossedPrice}`}>{currentPrice}</div>
@@ -63,6 +58,13 @@ const ProductCard = ({ product, onClick }: { product: Product; onClick: (key: st
           ) : (
             <div className={`${styles.commonPriceClass} ${styles.price}`}>{currentPrice}</div>
           )}
+          <div className={styles.buttonCover}>
+            {productId ? (
+              <PulseLoader color="#6d972e" cssOverride={{ margin: 'auto' }} size={10} />
+            ) : (
+              <GreenButtonWithPlus disabled={originalGoods.has(id)} text="Add to Cart" handler={handler} />
+            )}
+          </div>
         </div>
       </div>
     </div>
