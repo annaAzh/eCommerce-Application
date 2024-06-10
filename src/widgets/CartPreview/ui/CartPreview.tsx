fdeabfe7db46845bada66d164b2de024aad874d2
Irina Fileva <irinkaskai@gmail.com>
2024-06-10 14:44:12 +0500

    feat: add message on the page when the cart is empty

--- src/widgets/CartPreview/ui/CartPreview.tsx
+++ src/widgets/CartPreview/ui/CartPreview.tsx
@@ -1,9 +1,10 @@
 import { FC, useEffect, useMemo } from 'react';
-import style from './CartPreview.module.css';
 import { useAppDispatch, useAppSelector } from 'shared/lib/hooks';
 import { LineItem, getCart, getExistCart } from 'entities/Cart';
 import { ProductToCard } from 'features/ManageCartItemRow';
 import { getAccessToken } from 'entities/User';
+import ImgKitten from 'shared/assets/img/kittenForCart.png';
+import style from './CartPreview.module.css';
 
 export const CartPreview: FC = () => {
   const { lineItems } = useAppSelector(getCart);
@@ -17,18 +18,26 @@ export const CartPreview: FC = () => {
 
   const addedProducts = useMemo(() => {
     if (!lineItems) return;
-    return lineItems.map((product: LineItem, index) => {
+    if (lineItems.length === 0) {
       return (
-        <li key={index} className={style.productCart}>
-          <ProductToCard product={product} />
-        </li>
+        <div className={style.cartEmpty}>
+          <h2 className={style.messageCartEmpty}>{'Your cart is empty'}</h2>
+          <img className={style.imgCartEmpty} src={ImgKitten}></img>
+        </div>
       );
-    });
+    }
+    return (
+      <ol className={style.productList}>
+        {lineItems.map((product: LineItem, index) => {
+          return (
+            <li key={index} className={style.productCart}>
+              <ProductToCard product={product} />
+            </li>
+          );
+        })}
+      </ol>
+    );
   }, [lineItems]);
 
-  return (
-    <div className={style.container}>
-      <ol className={style.productList}>{addedProducts}</ol>
-    </div>
-  );
+  return <div className={style.container}>{addedProducts}</div>;
 };
