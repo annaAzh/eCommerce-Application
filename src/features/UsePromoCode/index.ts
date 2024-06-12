9e20c60cbf2784fba19fc3f8d1c04d2984f3bedd
Irina Fileva <irinkaskai@gmail.com>
2024-06-12 16:41:09 +0500

    feat: add active promo codes to the main page

---
+++ src/features/UsePromoCode/index.ts
@@ -0,0 +1,9 @@
+export { PromoCode } from './model/types/promoCodesTypes';
+
+export { getPromoCodes } from './model/services/getPromoCodes';
+
+export { takePromoCodes } from './model/selectors/promoCodeSelectors';
+
+export { promoCodeReducer } from './model/slices/promoCodeSlice';
+
+export { PromoCodeSchema } from './model/types/promoCodesTypes';
---
+++ src/shared/types/discountTypes.ts
@@ -0,0 +1,6 @@
+export enum DISCOUNT {
+  HAPPY = '-10%',
+  CLEAN = '-15%',
+  PLAY = CLEAN,
+  TASTY = '-20%',
+}
