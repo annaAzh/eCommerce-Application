bbf752780c59bc144a315cca8ef0f4ff444820a4
Irina Fileva <irinkaskai@gmail.com>
2024-06-13 16:58:02 +0500

    feat: show sum without discount, sum discount sale, sum discount promo code

--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -3,19 +3,25 @@ import style from './Cart.module.css';
 import { ProductListForCart } from 'widgets/ProductListForCart';
 import { getCart } from 'entities/Cart';
 import { useAppSelector } from 'shared/lib/hooks';
-import { TotalListPrices } from 'widgets/TotalListPrices';
 import { getAccessToken } from 'entities/User';
+import { FieldApplyPromoCode } from 'features/UsePromoCode';
+import { PriceList } from 'features/ManageCartPrices';
 
 export const Cart: FC = () => {
   const token = useAppSelector(getAccessToken);
-  const { lineItems, totalPrice } = useAppSelector(getCart);
+  const { lineItems, totalPrice, version, id, discountCodes } = useAppSelector(getCart);
 
   return (
     <>
-      <div className={style.cover}>
-        {lineItems && token && <ProductListForCart lineItems={lineItems} token={token} />}
-        {totalPrice && <TotalListPrices totalPrice={totalPrice} />}
-      </div>
+      {lineItems && token && version && id && totalPrice && discountCodes && (
+        <div className={style.cover}>
+          <ProductListForCart lineItems={lineItems} token={token} />
+          <div className={style.pricesAndInput}>
+            <FieldApplyPromoCode version={version} token={token} id={id} discountCodes={discountCodes} />
+            <PriceList totalAmount={totalPrice} lineItems={lineItems} />
+          </div>
+        </div>
+      )}
     </>
   );
 };
