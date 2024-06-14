34ab6261b0bef2c29ae61406de4002ada8a65e0a
rRedq <redq_000@mail.ru>
2024-06-14 07:19:28 +0500

    feat: add category list

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,11 +1,11 @@
 import { FC } from 'react';
-import ImgKittens from 'shared/assets/img/kittensMain.png';
 import './Main.css';
+import { CategoryList } from 'widgets/CategoryList';
 
 export const Main: FC = () => {
   return (
     <div className="wrapper-main">
-      <img className="img-kittens" src={ImgKittens} alt="kittens"></img>
+      <CategoryList />
     </div>
   );
 };
