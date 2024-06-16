a7b9ee1c7133b3bdb801f32646cecc1b97b44ebe
annaAzh <110905191+annaAzh@users.noreply.github.com>
2024-06-16 21:30:42 +0300

    fix/RSS-ECOMM-4_27: fix cart icon button
    * fix: fix badge
    
    * fix: fix hover on cart icon at header

--- src/features/DisplayGoodsQuantity/ui/CartBadge.tsx
+++ src/features/DisplayGoodsQuantity/ui/CartBadge.tsx
@@ -4,11 +4,19 @@ import { FC } from 'react';
 import { useAppSelector } from 'shared/lib/hooks';
 import './CartBadge.css';
 
-const CartBadge: FC = () => {
+interface CartBadgeProps {
+  children: React.ReactNode;
+}
+
+const CartBadge: FC<CartBadgeProps> = ({ children }) => {
   const cart = useAppSelector(getCart);
   const totalCartQuantity = cart?.lineItems?.reduce((acc, item) => acc + item.quantity, 0) || 0;
 
-  return <Badge count={totalCartQuantity} size="small" style={{ backgroundColor: '#6d972e' }}></Badge>;
+  return (
+    <Badge count={totalCartQuantity} size="small" style={{ backgroundColor: '#6d972e' }}>
+      {children}
+    </Badge>
+  );
 };
 
 export { CartBadge };
